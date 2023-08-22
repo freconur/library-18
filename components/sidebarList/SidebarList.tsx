0afdd8d4982ff04361f170a60076343decce77e7
franco condori <freconur@gmail.com>
2023-08-22 15:55:48 -0500

    agregando estilos al sidebar

--- components/sidebarList/SidebarList.tsx
+++ components/sidebarList/SidebarList.tsx
@@ -9,35 +9,35 @@ interface Props {
 const SidebarList = ({showSidebar, setShowSidebar}:Props) => {
   return (
     <ul className='capitalize font-semibold  text-slate-200'>
-      <li className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer  rounded-md mt-2 capitalize   hover:bg-red-200 hover:text-gray-500 whitespace-nowrap">
+      <li className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer  rounded-md mt-2 capitalize   hover:bg-cyan-400 hover:text-gray-800 whitespace-nowrap">
         <Link onClick={() => setShowSidebar(!showSidebar)} href="/dashboard/estadisticas" className="w-56 p-2">
           <RiBarChart2Fill className="text-2xl block float-left" />
           <span className={`text-base flex-1 ml-2 font-semibold`}>estadisticas</span>
           {/* <span className={`text-base flex-1 ml-2 font-semibold  ${!openSidebar && "hidden"}`}>estadisticas</span> */}
         </Link>
       </li>
-      <li className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer  rounded-md mt-2 capitalize   hover:bg-red-200 hover:text-gray-500 whitespace-nowrap">
+      <li className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer  rounded-md mt-2 capitalize   hover:bg-cyan-400 hover:text-gray-800 whitespace-nowrap">
         <Link onClick={() => setShowSidebar(!showSidebar)} href="/dashboard/registro-de-productos" className="w-56 p-2">
           <RiDraftFill className="text-2xl block float-left" />
           <span className={`text-base flex-1 ml-2 font-semibold`}>Registro de producto</span>
           {/* <span className={`text-base flex-1 ml-2 font-semibold  ${!openSidebar && "hidden"}`}>estadisticas</span> */}
         </Link>
       </li>
-      <li className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer  rounded-md mt-2 capitalize   hover:bg-red-200 hover:text-gray-500 whitespace-nowrap">
+      <li className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer  rounded-md mt-2 capitalize   hover:bg-cyan-400 hover:text-gray-800 whitespace-nowrap">
         <Link onClick={() => setShowSidebar(!showSidebar)} href="/dashboard/registro-ventas" className="w-56 p-2">
           <RiMoneyDollarCircleFill className="text-2xl block float-left" />
           <span className={`text-base flex-1 ml-2 font-semibold`}> Punto de venta</span>
           {/* <span className={`text-base flex-1 ml-2 font-semibold  ${!openSidebar && "hidden"}`}>estadisticas</span> */}
         </Link>
       </li>
-      <li className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer  rounded-md mt-2 capitalize   hover:bg-red-200 hover:text-gray-500 whitespace-nowrap">
+      <li className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer  rounded-md mt-2 capitalize   hover:bg-cyan-400 hover:text-gray-800 whitespace-nowrap">
         <Link onClick={() => setShowSidebar(!showSidebar)} href="/dashboard/cargas-stock" className="w-56 p-2">
           <RiBarChart2Fill className="text-2xl block float-left" />
           <span className={`text-base flex-1 ml-2 font-semibold`}>Cargas de stock</span>
           {/* <span className={`text-base flex-1 ml-2 font-semibold  ${!openSidebar && "hidden"}`}>estadisticas</span> */}
         </Link>
       </li>
-      <li className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer  rounded-md mt-2 capitalize   hover:bg-red-200 hover:text-gray-500 whitespace-nowrap">
+      <li className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer  rounded-md mt-2 capitalize   hover:bg-cyan-400 hover:text-gray-800 whitespace-nowrap">
         <Link onClick={() => setShowSidebar(!showSidebar)} href="/dashboard/productos" className="w-56 p-2">
           <RiArchiveDrawerFill className="text-2xl block float-left" />
           <span className={`text-base flex-1 ml-2 font-semibold`}>Mis productos</span>
