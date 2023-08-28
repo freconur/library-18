71af3a9bac6a32a6375baf6d9111b32cbaa40cfa
franco condori <freconur@gmail.com>
2023-08-27 21:24:02 -0500

    fix

--- pages/dashboard/estadisticas.tsx
+++ pages/dashboard/estadisticas.tsx
@@ -59,22 +59,22 @@ const Estadisticas = () => {
   return (
     <div className="w-full">
       {/* <div className="h-altura rounded-t-lg"> */}
-      <div>pagina de estadisticas</div>
+      <h1 className="text-2xl font-semibold my-5">Estadisticas</h1>
       {/* <div>venta del dia : {dailySale ? dailySale : 0}</div> */}
       <div className="grid grid-cols-2 gap-5 w-full">
         <div className="w-full bg-gradient-to-l from-amber-200 to-amber-300 rounded-lg p-2 shadow-md">
-          <h2 className="text-white text-xl font-semibold capitalize">Daily</h2>
+          <h2 className="text-white text-xl font-semibold capitalize">Daily sales</h2>
           <div className="font-semibold">
-            <div className="text-blue-600">Venta del dia : <span className="text-blue-600 font-semibold">s/ {dailySale && dailySale.toFixed(2)}</span></div>
-            <div className="text-white">Tickets del dia : <span className="font-semibold">{dailyTicket && dailyTicket}</span></div>
-            <div className="text-slate-700">Ticket promedio : s/ {averageTicket ? averageTicket.toFixed(2) : 0}</div>
+            <div className="text-slate-700">Venta : <span className="text-slate-700 font-semibold">S/ {dailySale && dailySale.toFixed(2)}</span></div>
+            <div className="text-slate-700">Tickets : <span className="font-semibold">{dailyTicket && dailyTicket}</span></div>
+            <div className="text-slate-700">T. promedio : S/ {averageTicket ? averageTicket.toFixed(2) : 0}</div>
           </div>
         </div>
         <div className="w-full bg-gradient-to-l from-blue-400 to-blue-500 rounded-lg p-2 shadow-md">
-          <h2 className="text-white text-xl font-semibold capitalize ">Ingresos por ventas</h2>
+          <h2 className="text-white text-xl font-semibold capitalize ">Ingresos</h2>
           <div className="font-semibold">
-            <div className="text-slate-700 capitalize">{currentMonth()}: s/ {dataTotalSalesPerMonth}</div>
-            <div className="text-white">Ingresos 2023: s/ 580</div>
+            <div className="text-slate-700 capitalize">{currentMonth()} : S/ {dataTotalSalesPerMonth}</div>
+            <div className="text-slate-700">2023 : S/ 580</div>
           </div>
         </div>
         {/* <div className="w-full">
@@ -89,7 +89,7 @@ const Estadisticas = () => {
         </div> */}
       </div>
       <div className="mt-5">
-        <h2 className="text-gray-400 text-xl font-semibold capitalize mb-5">grafico lineal de ventas</h2>
+        <h2 className="text-gray-400 text-xl font-semibold capitalize mb-5">grafico lineal de ventas diario</h2>
         <div className="w-[99%]">
           <Line data={sales} />
         </div>
