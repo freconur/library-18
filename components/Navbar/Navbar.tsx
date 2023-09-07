b27cd3c07c0d77562eca6cc720635312cf88fa4d
franco condori <freconur@gmail.com>
2023-09-06 22:46:06 -0500

    add: agregando hooks de ouyside a la barra de busqueda y display al name de usuario

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,11 @@
 "use client"
 import { getAuth } from 'firebase/auth'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { app } from '../../firebase/firebase.config'
 import { RiMenuFill } from "react-icons/ri";
 import algoliasearch from 'algoliasearch/lite';
-import ModalProducts from '../../modals/products/ModalProducts';
-import ProductsFromSerach from '../ProductsFromSerach/ProductsFromSerach';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 interface Props {
   showSidebar: boolean,
@@ -26,8 +25,9 @@ const index = searchClient.initIndex(ALGOLIA_INDEX)
 
 
 const Navbar = ({ showSidebar, setShowSidebar }: Props) => {
+  const closeBoxSearch = useRef<HTMLDivElement>(null)
+
   const [userInfo, setUserInfo] = useState<UserInfo>()
-  const [showModalProducts, setShowModalProducts] = useState(false)
   const authUser = getAuth()
   const auth = getAuth(app)
 
@@ -35,16 +35,32 @@ const Navbar = ({ showSidebar, setShowSidebar }: Props) => {
   const [conditionalValue, setConditionalValue] = useState(initialValueInput)
   const [results, setResults] = useState<any>(null)
 
+  const handleClickOutside = () => {
+    setConditionalValue(initialValueInput)
+  }
+  useOnClickOutside(closeBoxSearch, handleClickOutside)
+
   useEffect(() => {
     authUser.onAuthStateChanged(user => {
-      setUserInfo({ id: `${user?.uid}`, photo: `${user?.photoURL}`, name: `${user?.displayName}` })
+      let char = []
+      let names = ""
+      if (user?.displayName) {
+        const toString: string = `${user?.displayName}`
+        for (var i = 0; i < toString.length; i++) {
+          if (toString[i] === " ") {
+            names = char.join('')
+            return names
+          } else {
+            char.push(toString[i])
+          }
+        }
+        setUserInfo({ id: `${user?.uid}`, photo: `${user?.photoURL}`, name: `${names}` })
+        // setUserInfo({ id: `${user?.uid}`, photo: `${user?.photoURL}`, name: `${user?.displayName}` })
+      }
+      console.log('names',names)
     }
     )
   }, [results])
-
-  const modalProducts = () => {
-    setShowModalProducts(true)
-  }
 
   const handleChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
@@ -78,6 +94,16 @@ const Navbar = ({ showSidebar, setShowSidebar }: Props) => {
   const testData = (item: string) => {
     return { __html: item }
   }
+
+  const testEnter = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log(e.key)
+    if (e.key === 'Escape') {
+      handleClickOutside()
+    }
+    new KeyboardEvent('keydown', {
+      'key': 'Tab'
+    })
+  }
   return (
     <>
       <nav className='relative w-full h-[60px] px-2 bg-white shadow-md flex justify-between items-center p-1 rounded-b-lg'>
@@ -88,10 +114,12 @@ const Navbar = ({ showSidebar, setShowSidebar }: Props) => {
 
         <div className='w-full mx-5 relative z-600'>
           <input
+            onKeyDown={testEnter}
             name="description"
             onChange={handleChangeValue}
             className='border-[1px] w-full border-slate-200 rounded-sm h-[40px] pl-3 text-slate-500 p-1'
             type="text"
+            value={conditionalValue.description}
           />
         </div>
         <div className='flex justify-center items-center'>
@@ -101,21 +129,32 @@ const Navbar = ({ showSidebar, setShowSidebar }: Props) => {
         {
           conditionalValue.description.length > 0
             ?
-            <div className='shadow-lg absolute top-[40px] right-0 md:right-[152px] left-0 md:top-[40px] md:left-[88px] mx-1 my-1 p-2 bg-white rounded-sm'>
+            <div ref={closeBoxSearch} className=' shadow-lg absolute top-[40px] right-0 md:right-[152px] left-0 md:top-[40px] md:left-[88px] mx-1 my-1 p-2 bg-white rounded-sm'>
               <h3 className='text-slate-700 capitalize font-dmMono text-lg my-5'>productos relacionados</h3>
               {/* <ProductsFromSerach results={results}/> */}
-              <div>
+              <div className='overflow-y-scroll h-[500px] pr-2'>
                 {
                   results &&
                   results.map((item: ProductToCart) => {
                     return (
                       <CopyToClipboard key={item.code} text={item.code as string}>
                         <div className='w-full my-2 border-b-2 cursor-pointer border-slate-100'>
-                          <p className='text-slate-600 font-nunito'>{item.code}</p>
+                          <div className='flex justify-between items-center'>
+                            <p className='text-slate-600 capitalize font-nunito' dangerouslySetInnerHTML={testData(`${item.code}`)} />
+
+                            {/* <p className='text-slate-600 font-nunito'>{item.code}</p> */}
+                            <p className='text-slate-600 font-nunito'>stock: {item.stock}</p>
+                          </div>
                           <h3 className='text-slate-600 capitalize font-nunito' dangerouslySetInnerHTML={testData(`${item.description}`)} />
                           <div className='flex justify-between items-center'>
-                            <p className='text-slate-600 font-nunito'>marca: {item.brand}</p>
-                            <p className='text-slate-600 font-nunito'>precio: $ {item.price}</p>
+                            <div className='flex'>
+                              <span className='font-nunito text-slate-600'>marca: </span><p className='text-slate-600 capitalize font-nunito' dangerouslySetInnerHTML={testData(`${item.brand}`)} />
+                            </div>
+                            <div className='flex'>
+                              <span className='font-nunito text-slate-600'>precio: $</span><p className='text-slate-600 capitalize font-nunito' dangerouslySetInnerHTML={testData(`${item.price}`)} />
+                            </div>
+                            {/* <p className='text-slate-600 font-nunito'>marca: {item.brand}</p> */}
+                            {/* <p className='text-slate-600 font-nunito'>precio: $ {item.price}</p> */}
                           </div>
                         </div>
                       </CopyToClipboard>
