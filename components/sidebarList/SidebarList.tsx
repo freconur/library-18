c821c86ca50334313ea389cfa07b7ad9a4d123c4
franco condori <freconur@gmail.com>
2023-10-12 17:50:28 -0500

    fix:cmabiando nombre de tickets de la lista de sidebar

--- components/sidebarList/SidebarList.tsx
+++ components/sidebarList/SidebarList.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
-import { RiBarChart2Fill, RiMoneyDollarCircleFill, RiDraftFill, RiArchiveDrawerFill, RiArrowLeftSLine } from "react-icons/ri";
+import { RiBarChart2Fill, RiArrowLeftSLine } from "react-icons/ri";
 import { MdPointOfSale } from "react-icons/md";
 import { BsFillBoxFill } from "react-icons/bs";
 import { BiArchiveOut } from "react-icons/bi";
 import { useGlobalContext } from '../../context/GlobalContext';
+import { TiTicket } from "react-icons/ti";
 interface Props {
   showSidebar: boolean,
   setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>,
   sidebarProducts: () => void
 }
-const SidebarList = ({ sidebarProducts, setShowSidebar }: Props) => {
+const SidebarList = ({ sidebarProducts }: Props) => {
   const { showSidebarContext, LibraryData } = useGlobalContext()
   const { showSidebar } = LibraryData
   return (
@@ -19,7 +20,6 @@ const SidebarList = ({ sidebarProducts, setShowSidebar }: Props) => {
         <Link onClick={() => showSidebarContext(!showSidebar)} href="/dashboard/estadisticas" className="my-3 w-56 p-2">
           <RiBarChart2Fill className="text-slate-600 text-xl block float-left" />
           <span className={`text-base flex-1 ml-2 text-md text-slate-500`}>estadisticas</span>
-          {/* <span className={`text-base flex-1 ml-2   ${!openSidebar && "hidden"}`}>estadisticas</span> */}
         </Link>
       </li>
       <li className="text-gray-300 border-b-[1px] border-slate-300 text-sm flex items-center gap-x-4 cursor-pointer   mt-2 capitalize   hover:bg-slate-200 duration-300 hover:text-gray-800 whitespace-nowrap my-3">
@@ -29,7 +29,6 @@ const SidebarList = ({ sidebarProducts, setShowSidebar }: Props) => {
           <BsFillBoxFill className="text-slate-600 text-xl block float-left" />
           <span className={`text-base flex-1 ml-2 text-md text-slate-500`}>productos</span>
 
-          {/* <span className={`text-base flex-1 ml-2   ${!openSidebar && "hidden"}`}>estadisticas</span> */}
         </Link>
         <Link onClick={sidebarProducts} href="">
         <RiArrowLeftSLine className='rotate-180 text-2xl text-slate-600 w-[40px]' />
@@ -40,26 +39,21 @@ const SidebarList = ({ sidebarProducts, setShowSidebar }: Props) => {
 
       <li className="text-gray-300 border-b-[1px] border-slate-300 text-sm flex items-center gap-x-4 cursor-pointer   mt-2 capitalize   hover:bg-slate-200 duration-300 hover:text-gray-800 whitespace-nowrap my-3">
         <Link onClick={() => showSidebarContext(!showSidebar)} href="/dashboard/registro-ventas" className="my-3 w-56 p-2">
-          {/* <RiMoneyDollarCircleFill className="text-slate-600 text-xl block float-left" /> */}
           <MdPointOfSale className="text-slate-600 text-xl block float-left" />
           <span className={`text-base flex-1 ml-2 text-md text-slate-500`}> Punto de venta</span>
-          {/* <span className={`text-base flex-1 ml-2   ${!openSidebar && "hidden"}`}>estadisticas</span> */}
+        </Link>
+      </li>
+      
+      <li className="text-gray-300 border-b-[1px] border-slate-300 text-sm flex items-center gap-x-4 cursor-pointer   mt-2 capitalize   hover:bg-slate-200 duration-300 hover:text-gray-800 whitespace-nowrap my-3">
+        <Link onClick={() => showSidebarContext(!showSidebar)} href="/dashboard/anulacion-venta" className="my-3 w-56 p-2">
+          <TiTicket className="text-slate-600 text-xl block float-left" />
+          <span className={`text-base flex-1 ml-2 text-md text-slate-500`}>Mis tickets</span>
         </Link>
       </li>
       <li className="text-gray-300 border-b-[1px] border-slate-300 text-sm flex items-center gap-x-4 cursor-pointer   mt-2 capitalize   hover:bg-slate-200 duration-300 hover:text-gray-800 whitespace-nowrap my-3">
         <Link onClick={() => showSidebarContext(!showSidebar)} href="/dashboard/ventas" className="my-3 w-56 p-2">
-          {/* <RiMoneyDollarCircleFill className="text-slate-600 text-xl block float-left" /> */}
           <BiArchiveOut className="text-slate-600 text-xl block float-left" />
           <span className={`text-base flex-1 ml-2 text-md text-slate-500`}>Productos vendidos</span>
-          {/* <span className={`text-base flex-1 ml-2   ${!openSidebar && "hidden"}`}>estadisticas</span> */}
-        </Link>
-      </li>
-      <li className="text-gray-300 border-b-[1px] border-slate-300 text-sm flex items-center gap-x-4 cursor-pointer   mt-2 capitalize   hover:bg-slate-200 duration-300 hover:text-gray-800 whitespace-nowrap my-3">
-        <Link onClick={() => showSidebarContext(!showSidebar)} href="/dashboard/anulacion-venta" className="my-3 w-56 p-2">
-          {/* <RiMoneyDollarCircleFill className="text-slate-600 text-xl block float-left" /> */}
-          <MdPointOfSale className="text-slate-600 text-xl block float-left" />
-          <span className={`text-base flex-1 ml-2 text-md text-slate-500`}>anulacion de venta</span>
-          {/* <span className={`text-base flex-1 ml-2   ${!openSidebar && "hidden"}`}>estadisticas</span> */}
         </Link>
       </li>
     </ul>
